163bd21d3ffde2bd97a89d7d81f1c576b73f4a98
Ussf-nassrallah <nassrallah.dev@gmail.com>
2024-01-24 12:10:46 +0100

    improve Cache class

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 ''' 0x02. Redis basic with python '''
 
-from typing import Union
+from typing import Union, Callable
 import uuid
 import redis
 
@@ -17,3 +17,24 @@ class Cache:
         ran_key = str(uuid.uuid4())
         self._redis.set(ran_key, data)
         return ran_key
+
+    def get(
+        self,
+        key: str,
+        fn: Callable = None
+    ) -> Union[str, bytes, int, float]:
+        ''' get Method '''
+        result = self._redis.get(key)
+        if fn is not None:
+            return fn(result)
+        return result
+
+    def get_str(self, key: str) -> str:
+        ''' get string value '''
+        result = self.get(key, lambda x: x.decode('utf-8'))
+        return result
+
+    def get_int(self, key: str) -> int:
+        ''' get int value '''
+        result = self.get(key, lambda x: int(x))
+        return result
